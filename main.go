2aff78f4276dfbc7b46b406583db95a76f418fdd
Nate <nate@pop-os.localdomain>
2022-10-03 12:05:41 -0600

    adding deck logic

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 	
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	hand, remaingCards := deal(cards, 5)
 
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	hand.print()
+	remaingCards.print()
 }
