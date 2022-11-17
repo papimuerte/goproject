97f6b7040196c0f1c7d31300547bdd590c66f9e8
papimuerte <nate@nateward.net>
2022-11-17 10:45:20 -0700

    more tests

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -8,4 +11,19 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 20, but got %v", len(d))
 	}
+}
+
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module nateward.net/m/v2
+
+go 1.18
